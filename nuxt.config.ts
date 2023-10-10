65458d27213f9458e2db1a574ebece8052f7713f
Ivan Novak <ispeakwithdead@gmail.com>
2023-10-10 19:16:16 +0200

    fix fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/google-fonts', "@nuxtjs/tailwindcss", '@vueuse/nuxt', '@nuxt/image', 'nuxt-icons'],
-  googleFonts: {
-    subsets: 'cyrillic',
-    families: {
-      Manrope: true,
-    }
-  },
+  modules: ["@nuxtjs/tailwindcss", '@vueuse/nuxt', '@nuxt/image', 'nuxt-icons'],
   css: ["~/assets/css/main.css"],
   postcss: {
     plugins: {
