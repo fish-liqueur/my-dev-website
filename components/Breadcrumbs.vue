c9377d0a80ffa77387b89c8e301d633a56a30fd5
Ivan Novak <ispeakwithdead@gmail.com>
2023-12-10 02:04:16 +0100

    add breadcrumbs

---
+++ components/Breadcrumbs.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+    const route = useRoute();
+</script>
+
+<template>
+    <div v-if="route.path !== '/'" class="flex flex-wrap gap-2 py-2">
+        <NuxtLink to="/">home</NuxtLink>
+        <span class="text-gray-700">/ {{ route.name }}</span>
+    </div>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="flex flex-col min-h-screen bg-neutral-200">
         <HeaderMain />
+        <Breadcrumbs class="self-center"/>
         <main class="grow max-w-6xl px-3 mx-auto">
             <slot />
         </main>
